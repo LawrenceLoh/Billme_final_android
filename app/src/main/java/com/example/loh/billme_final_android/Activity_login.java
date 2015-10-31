6d1c84a719d43964801ad345820396fde81c340d
LawrenceLoh <xin08289@gmail.com>
2015-10-31 22:00:46 +0800

    login

--- app/src/main/java/com/example/loh/billme_final_android/Activity_login.java
+++ app/src/main/java/com/example/loh/billme_final_android/Activity_login.java
@@ -1,16 +1,30 @@
 package com.example.loh.billme_final_android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class Activity_login extends AppCompatActivity {
+public class Activity_login extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btn_login = (Button)findViewById(R.id.login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity_login.this,Activity_mainPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 
 }
