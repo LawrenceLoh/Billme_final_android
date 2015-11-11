fece0c12cc8c68d7e51e2d573f9e8593c6d3c612
LawrenceLoh <xin08289@gmail.com>
2015-11-11 22:50:01 +0800

    join group

--- app/src/main/java/com/example/loh/billme_final_android/Activity_group.java
+++ app/src/main/java/com/example/loh/billme_final_android/Activity_group.java
@@ -6,23 +6,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.loh.billme_final_android.Parse_subclass.Group;
-import com.example.loh.billme_final_android.Parse_subclass.Invite;
 import com.example.loh.billme_final_android.Parse_subclass.User;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
-
-import junit.framework.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,26 +26,19 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class Activity_group_list extends AppCompatActivity {
+public class Activity_group extends AppCompatActivity {
 
-    @Bind(R.id.group_list)ListView listView_group_list;
-    final List<Group> memberList= new ArrayList<Group>();
+    private String groupname;
+    @Bind(R.id.group_members_list)ListView group_members_list;
+    final List<User> memberList= new ArrayList<User>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity_group_list);
+        setContentView(R.layout.activity_activity_group);
+        groupname= getIntent().getStringExtra("groupname");
+        Toast.makeText(Activity_group.this, groupname, Toast.LENGTH_SHORT).show();
         ButterKnife.bind(this);
-        onLoadingGroupList();
-
-        listView_group_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(Activity_group_list.this,Activity_group.class);
-                intent.putExtra("groupname",memberList.get(position).getGroupName());
-                startActivity(intent);
-                finish();
-            }
-        });
+        onLoadingGroupMember();
     }
 
     @Override
@@ -73,7 +62,7 @@ public class Activity_group_list extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.menu_back) {
-            Intent intent = new Intent(Activity_group_list.this,Activity_main.class);
+            Intent intent = new Intent(Activity_group.this,Activity_main.class);
             startActivity(intent);
             finish();
             return true;
@@ -82,17 +71,18 @@ public class Activity_group_list extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onLoadingGroupList(){
+    public void onLoadingGroupMember(){
 
         ParseQuery<Group> group_list = ParseQuery.getQuery(Group.class);
-        group_list.whereEqualTo("member", ParseUser.getCurrentUser());
+        group_list.whereEqualTo("groupName", groupname);
+        group_list.include("member");
         group_list.findInBackground(new FindCallback<Group>() {
             @Override
             public void done(List<Group> list, ParseException e) {
                 for(Group member :list){
-                    memberList.add(member);
+                    memberList.add(member.getMember());
                 }
-                listView_group_list.setAdapter(new Adapter_group_list(getApplicationContext(),list));
+                group_members_list.setAdapter(new Adapter_search_user(getApplicationContext(), memberList, memberList.size()));
             }
         });
     }
